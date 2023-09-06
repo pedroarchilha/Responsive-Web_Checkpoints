9f784eb8c1cb4b823f9a0ba4c3c25aa5afb02b83
pedroarchilha <rm550450@fiap.com.br>
2023-09-06 17:44:51 -0300

    Fazendo comentarios  na rota Produtos.

--- Exercicio_06.09/src/routes/Produtos/index.jsx
+++ Exercicio_06.09/src/routes/Produtos/index.jsx
@@ -1,19 +1,18 @@
-import { Link } from "react-router-dom"
-import { listaProdutos } from "../../components/listaProdutos"
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../components/listaProdutos";
 
-
-export default function Produtos()
-{
-    return(
+export default function Produtos() {
+    return (
         <main>
             <h1>Produtos</h1>
-            {listaProdutos.map(prod => (
+            {/* isso aqui eh tpw um for. Vai percorrer a lista de produtos, e mostrar os items com a rota /prod.id */}
+            {listaProdutos.map((prod) => (
                 <div key={prod.id}>
-                <Link to={`/produtos/editar/${prod.id}`}>
-                    Editar o produto: {prod.nome}
-                </Link>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
                 </div>
             ))}
         </main>
-    )
+    );
 }
